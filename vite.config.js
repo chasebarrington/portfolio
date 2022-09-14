046019064f90f923a6e3afbc91d8fc1cc78a2b08
tripp <tripp@gmail.com>
2022-09-14 13:51:11 -0400

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import vue from '@vitejs/plugin-vue'
 import viteCompression from 'vite-plugin-compression';
 
 export default {
+  base: '/portfolio/',
   plugins: [vue(), viteCompression(), viteCompression({algorithm: 'brotliCompress'})]
 }
